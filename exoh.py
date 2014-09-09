ecbbc1b2cfebc1ce3cbcf5e4a9beafc3e695e6ab
modzozo <modzozo@users.noreply.github.com>
2014-09-09 21:08:36 +0300

    commit exoh.py
    first task of hackbulgaria 101

---
+++ exoh.py
@@ -0,0 +1,12 @@
+#The function should return true if there is an equal number of x's and o's in str. It should return false otherwise.You don't have to check for valid input
+request = raw_input(" Please enter only a string of x's and o's: ")
+
+def ExOh(str):
+    count_x = request.count("x")
+    count_o = request.count("o")
+    if count_x == count_o:
+	print True
+    else:
+	print False
+
+ExOh(request)
