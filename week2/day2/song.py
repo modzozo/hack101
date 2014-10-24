2e53e19b28f97301e27148222f94b426f95e65cd
modzozo <modzozo@abv.bg>
2014-10-24 19:09:16 +0300

    add song class

---
+++ week2/day2/song.py
@@ -0,0 +1,19 @@
+
+class Song:
+
+    MAX_RATING = 5
+    MIN_RATING = 1
+
+    def __init__(self,title,artist,album,rating,length,bitrate):
+        self.title = title
+        self.artist = artist
+        self.album = album
+        self.rating = rating
+        self.length = length
+        self.bitrate = bitrate
+
+    def rate(self, value):
+        if value < Song.MIN_RATING or value > Song.MAX_RATING:
+
+            raise ValueError
+        self.rating = value
