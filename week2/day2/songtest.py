1ddc49daa8831cf830e22993a6818491bf2be02f
modzozo <modzozo@abv.bg>
2014-10-24 19:11:43 +0300

    fix length MIN:SEC syntax

--- week2/day2/songtest.py
+++ week2/day2/songtest.py
@@ -4,14 +4,14 @@ from song import Song
 class SongTest(unittest.TestCase):
 
     def setUp(self):
-        self.song = Song("Shelter", "Years & Years","EP",4,4,128)
+        self.song = Song("Shelter", "Years & Years","EP",4,"04:00",128)
 
     def test_init(self):
         self.assertEqual(self.song.title, "Shelter")
         self.assertEqual(self.song.artist, "Years & Years")
         self.assertEqual(self.song.album,"EP")
         self.assertEqual(self.song.rating,4)
-        self.assertEqual(self.song.length,4)
+        self.assertEqual(self.song.length,"04:00")
         self.assertEqual(self.song.bitrate,128)
 
     def test_rate_outofvalue(self):
